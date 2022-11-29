d5e9e0487e85d56266baf445c0a77af6bf585a26
Michael Ovdiienko <mishaov@gmail.com>
2022-11-29 15:44:06 +0200

    feat: initial commit

---
+++ src/List.js
@@ -0,0 +1,13 @@
+function List() {
+    return (
+        <div className="">
+            <header className="">
+                <p>
+                    Kek
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default List;
