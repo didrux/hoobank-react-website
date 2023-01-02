9b33c9bbd25a1f9a717317be414ee36533c9a5a5
Michael Ovdiienko <mishaov@gmail.com>
2023-01-03 00:51:21 +0200

    Revert "chore: add resume file"
    This reverts commit 49e46b25f151225c9d76f4ca40d9d54f43f54ece.

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,31 @@ import Features from "./components/Features";
 import Profile from "./components/Profile.jsx";
 import Login from "./components/Login.jsx";
 import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
-import Gallery from "react-photo-gallery";
 
 const App = () => {
-    const photos = [
-        {
-            src: "src/img/che-snow/1.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/2.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/3.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/4.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/5.JPG",
-            width: 1,
-            height: 1
-        },
-        {
-            src: "src/img/che-snow/6.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/7.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/8.JPG",
-            width: 4,
-            height: 3
-        },
-        {
-            src: "src/img/che-snow/9.JPG",
-            width: 4,
-            height: 3
-        },
-    ];
-    return (
-    <div className="">
-      <Gallery photos={photos} />;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Hero />}></Route>
+        <Route path="/home" element={<Hero />}></Route>
+        <Route path="/features" element={<Features />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
